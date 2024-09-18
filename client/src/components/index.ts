7d82ef594c126260b804d636a34b02c2aa9e4252
Hassaan-Bin-Kaleem <hassaanbinkaleem216@gmail.com>
2024-09-18 23:56:46 +0500

    Profile Card

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,5 @@
 import Navbar from "./navbar/Navbar";
-import ProfileCard from "./card/ProfileCard";
+import ProfileCard from "./profileCard/ProfileCard";
 import ContactForm from "./contact/ContactForm";
 
 export { Navbar, ProfileCard, ContactForm };
